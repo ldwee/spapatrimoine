25d9d2c1ce453f28b79bf025c02a3f4289a617da
luc de weerdt <luc@dewetho.be>
2020-11-02 09:12:00 +0100

    link activity_type to activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_190636) do
+ActiveRecord::Schema.define(version: 2020_10_29_204812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_10_28_190636) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "image"
+    t.bigint "activity_type_id"
+    t.index ["activity_type_id"], name: "index_activities_on_activity_type_id"
   end
 
   create_table "activity_types", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_10_28_190636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "activity_types"
 end
