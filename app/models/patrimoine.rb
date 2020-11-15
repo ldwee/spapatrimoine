300bb037126b43ae5ed7727ef92d85673475dbc8
Loic Boset <loic.boset@gmail.com>
2020-11-15 13:53:12 +0100

    Seeding patrimoine

--- app/models/patrimoine.rb
+++ app/models/patrimoine.rb
@@ -1,11 +1,14 @@
 class Patrimoine < ApplicationRecord
-    belongs_to :contributor
-    has_many :activities
-    has_many :actualities
+  belongs_to :contributor
 
-    validates :libelle, presence: true
-    validates :localisation, presence: true
-    validates :notice, presence: true
-    validates :etat, presence: true
-    validates :status, presence: true, inclusion: { in: %w(attente acceptée refusée)}
+  has_many :activities
+  has_many :actualities
+
+  has_many_attached :images
+
+  validates :libelle, presence: true
+  validates :localisation, presence: true
+  validates :notice, presence: true
+  validates :etat, presence: true
+  validates :status, presence: true, inclusion: { in: %w(attente acceptée refusée)}
 end
